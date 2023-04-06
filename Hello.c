d17b30daf76166789d8f9cb1a90d8a67acd2ac95
Pratikg69 <130055541+Pratikg69@users.noreply.github.com>
2023-04-06 16:56:10 +0530

    pratik editing first csx

--- Hello.c
+++ Hello.c
@@ -5,6 +5,10 @@ int main()
     char str[40]; // declare the size of character string  
     printf (" \n Enter a string to be reversed: ");  
     scanf ("%s", str);  
+    int a=16;
+    int b=89;
+    
+    print("Pappaisback");
       
     // use strrev() function to reverse a string  
     printf (" \n After the reverse of a string: %s ", strrev(str));  
